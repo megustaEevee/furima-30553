6391d98622f80137eb6a5f4aba11c22886cd7fa5
megustaEevee <kotori_cat@yahoo.co.jp>
2020-10-10 23:35:28 +0900

    エラーハンドリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   has_many :purchasers
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
   validates :birthday, presence: true
   
   devise :database_authenticatable, :registerable,
