1bd933882127423831707a4f151bceb5e4f90b84
megustaEevee <kotori_cat@yahoo.co.jp>
2020-10-12 21:41:20 +0900

    ActiveHashの導入とエラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,20 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
 
   def new
+    @item = Item.new
   end
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:info,:category_id,:sales_status_id,:shipping_fee_status_id,:prefecture_source_id,:scheduled_delivery_id,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,20 @@ class Item < ApplicationRecord
 
   validates :name,                   presence: true
   validates :info,                   presence: true
-  validates :category_id,            presence: true
-  validates :sales_status_id,        presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_source_id,   presence: true
-  validates :scheduled_delivery_id,  presence: true
-  validates :price,                  presence: true
+  validates :category_id,            presence: true, numericality: { other_than: 1 }
+  validates :sales_status_id,        presence: true, numericality: { other_than: 1 }
+  validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1 }
+  validates :prefecture_source_id,   presence: true, numericality: { other_than: 0 }
+  validates :scheduled_delivery_id,  presence: true, numericality: { other_than: 1 }
+  validates :price,                  presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   validates :image, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :salesstatus
+  belongs_to_active_hash :shippingfeestatus
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduleddelivery
 
 end
