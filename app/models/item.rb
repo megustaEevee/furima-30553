931414d983b8e33ad3d5d11ca359df76ecf657d9
megustaEevee <kotori_cat@yahoo.co.jp>
2020-10-12 13:07:36 +0900

    itemモデルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one    :purchaser
+
+  validates :name,                   presence: true
+  validates :info,                   presence: true
+  validates :category_id,            presence: true
+  validates :sales_status_id,        presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_source_id,   presence: true
+  validates :scheduled_delivery_id,  presence: true
+  validates :price,                  presence: true
+
+end
