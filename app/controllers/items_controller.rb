3fbdcc66c7f9bb6c063b825282579f2a13cc6da5
megustaEevee <kotori_cat@yahoo.co.jp>
2020-10-15 21:00:31 +0900

    記述ミスの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,13 +24,15 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == @item.user.id
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
