02d769e60e2142a3f6ba2ebbd0e7626ccb0b56d5
megustaEevee <kotori_cat@yahoo.co.jp>
2020-10-16 11:04:20 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if
+    if current_user.id == @item.user.id
       @item.destroy
+      redirect_to root_path
+    else
       redirect_to root_path
     end
   end
