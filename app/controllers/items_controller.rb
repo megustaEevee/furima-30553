14f3e4b885801c10bf7a7caace4db74073b33ea5
megustaEevee <kotori_cat@yahoo.co.jp>
2020-10-16 09:40:52 +0900

    destroyアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_item , only: [:show, :edit, :update]
+  before_action :find_item , only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(id: :DESC)
@@ -33,6 +33,13 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item.id)
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if
+      @item.destroy
+      redirect_to root_path
     end
   end
 
